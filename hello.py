925ac7d458b56633073926086c3c83d94bb31efd
Alex Ladda <alex.ladda@gmail.com>
2021-03-20 21:11:29 +0800

    added ./templates

--- hello.py
+++ hello.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template("home.html", pages=["page1", "page2", "page3"])
+
+@app.route('/pages/')
+def pages():
+    return 'here are pages'
+
+@app.route('/projects/')
+def projects():
+    return 'here are projects'
+
+@app.route('/about/')
+def about():
+    return 'about'
+
+@app.route('/<search_query>')
+def search(search_query):
+    return render_template("search.html", content=search_query)
+
+@app.route("/confidential")
+def confidential():
+    #FEATURE: add confindential content
+    #TODO: this ends in a redirect loop
+    return redirect(url_for("search", search_query="confidential"))
+
+if __name__ == "__main__":
+    app.run()
