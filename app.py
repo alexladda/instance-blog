8edfe9f5b7b0e74dd18a7f21b489026cdcd3c692
Alex Ladda <alex.ladda@gmail.com>
2021-04-23 11:30:12 +0800

    minifiy html/css/js

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, redirect, url_for, render_template, abort, Response
 from flaskext.markdown import Markdown
-# from flask_minify import minify
 import pathlib
 import os
 from flask import send_from_directory
 from matplotlib.figure import Figure
 import random
+from htmlmin.minify import html_minify
+from csscompressor import compress
+from jsmin import jsmin
 
+# from flask_minify import minify
 
 import include.weather_station as ws
 
@@ -40,16 +43,30 @@ def get_content(category):
 def utility_processor():
     # inline CSS and Javascript
     def get_css():
-        '''
+        """
         collects all styles from the static directory into one string
-        '''
+        """
         styles = ["normalize.css", "milligram.css", "main.css", "nav.css"]
         css = ""
-        for style in styles:
-            path = "static/" + style
+        for file in styles:
+            path = "static/" + file
             with open(path) as f:
                 css = css + f.read()
+        css = compress(css)
         return css
+
+    def get_js():
+        """
+        collects all scripts from the static directory into one string
+        """
+        scripts = ["nav.js"]
+        js = ""
+        for file in scripts:
+            path = "static/" + file
+            with open(path) as f:
+                js = js + f.read()
+        js = jsmin(js)
+        return js
 
     def test_plot():
         fig = Figure()
@@ -59,7 +76,15 @@ def utility_processor():
         axis.plot(xs, ys)
         return fig.svg()
 
-    return dict(get_css=get_css, test_plot=test_plot)
+    def pages_nav():
+        pass
+
+    def projects_nav():
+        pass
+
+    return dict(get_css=get_css,
+                get_js=get_js,
+                test_plot=test_plot)
 
 
 def get_elements():
@@ -114,12 +139,13 @@ def robots():
 def home():
     # passing all pages to render navigation in base.html
     elements = get_elements()
-    return render_template("home.html",
-                           nav_pages=elements['pages'],
-                           nav_projects=elements['projects'])
+    rendered_html = render_template("home.html",
+                                    nav_pages=elements['pages'],
+                                    nav_projects=elements['projects'])
+    return html_minify(rendered_html)
 
 
-@app.route('/<page>')
+@app.route('/<page>/')
 def page(page):
     path = "pages/" + page + ".md"
     file = pathlib.Path(path)
@@ -134,16 +160,16 @@ def page(page):
                                nav_pages=elements['pages'],
                                nav_projects=elements['projects'])
     else:
-        return redirect(url_for("search", search_query=page))
+        abort(404)
 
 
 @app.route('/projects/')
-def projects():
+def projects_archive():
     # passing all pages and projects to render navigation in base.html
     # TODO: Archive page to aggregate all Projects
     elements = get_elements()
-    markup = get_markup('projects', '__archive')
-    return render_template("search.html",
+    markup = get_markup('projects/', '__archive')
+    return render_template("page.html",
                            page_body=markup,
                            list=elements['projects'],
                            page_title='projects',
@@ -151,8 +177,8 @@ def projects():
                            nav_projects=elements['projects'])
 
 
-@app.route('/projects/<project>')
-def project(project):
+@app.route('/projects/<project>/')
+def projects(project):
     path = "projects/" + project + ".md"
     file = pathlib.Path(path)
     elements = get_elements()
@@ -165,31 +191,31 @@ def project(project):
                                nav_pages=elements['pages'],
                                nav_projects=elements['projects'])
     else:
-        return redirect(url_for("search", search_query=page))
+        abort(404)
 
 
-@app.route('/search/<search_query>')
-def search(search_query):
-    # FEATURE: add site search
-    # TODO: everything
-    elements = get_elements()
-    content = 'Looks like you were looking for <em>{}</em>.'.format(search_query)
-    return render_template("search.html",
-                           content=content,
-                           list=['dummy', 'dummy', 'dummy'],
-                           page_title='site search',
-                           nav_pages=elements['pages'],
-                           nav_projects=elements['projects'])
+# @app.route('/search/<search_query>')
+# def search(search_query):
+#     # FEATURE: add site search
+#     # TODO: everything
+#     elements = get_elements()
+#     content = 'Looks like you were looking for <em>{}</em>.'.format(search_query)
+#     return render_template("search.html",
+#                            content=content,
+#                            list=['dummy', 'dummy', 'dummy'],
+#                            page_title='site search',
+#                            nav_pages=elements['pages'],
+#                            nav_projects=elements['projects'])
 
 
-@app.route("/confidential")
-def confidential():
-    # FEATURE: add confindential content
-    # TODO: everything
-    return redirect(url_for("search", search_query="confidential"))
+# @ app.route("/confidential/")
+# def confidential():
+#     # FEATURE: add confindential content
+#     # TODO: everything
+#     return redirect(url_for("search", search_query="confidential"))
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
     elements = get_elements()
--- static/nav.js
+++ static/nav.js
@@ -1,1 +1,20 @@
-// here be dragons
+function hamburger() {
+  var x = document.getElementById("main_nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function set_nav() {
+  // mobile: #nav {display: none;}
+  // desktop: #nav {display: block;}
+  var w = window.outerWidth;
+  var x = document.getElementById("main_nav");
+  if (w > 800) {
+    x.style.display = "block";
+  } else {
+    // this also tends to conveniently hide the nav
+    x.style.display = "none";
+  }
+}
